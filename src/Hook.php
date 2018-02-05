17475127a79a90bbfdc32824082fb161ed68ac56
avantis <uaruscom@gmail.com>
2018-02-05 19:09:43 +0200

    Add files via upload

--- src/Hook.php
+++ src/Hook.php
@@ -21,27 +21,40 @@ class Hook
     private $config;
     protected $request;
     protected $response;
-    protected $args = [];
+    protected $args;
     protected $view = [];
     protected $render = null;
-    protected $resource = null;
     protected $name_db = null;
     protected $query = null;
     protected $app = null;
+	protected $routers = null;
+	protected $resource = null;
     protected $postArr = [];
     protected $postQuery = null;
     protected $id = null;
     protected $callback = null;
     protected $hooks = null;
     protected $logger = null;
+	protected $print = null;
 	private $path = __DIR__ . '/';
  
     function __construct($config = [])
     {
         if(isset($config)) {
 			$this->config = $config;
+			if(isset($this->config['hooks']['print'])) {
+			    $this->print = $this->config['hooks']['print'];
+			}
+			if((int)$this->print == 1) {
+			     print("Config из конструктора<br>");
+			}
 		} else {
+		    
 		    $this->config = $this->get_config();
+			$this->print = $this->config['hooks']['print'];
+			if((int)$this->print == 1) {
+				print("Config из файла hooks.json<br>");
+			}
 		}
     }
  
@@ -57,13 +70,23 @@ class Hook
 		return json_decode($this->path.'/hooks.json', true);
 	}
  
-    public function http(Request $request, Response $response, array $args, $query = null, $app = null)
+    public function http(Request $request, Response $response, array $args, $query = null, $app = null, $routers = null, $resource = null)
     {
-        $this->args = $args;
         $this->request = $request;
         $this->response = $response;
-        $this->query = $query;
-        $this->app = $app;
+		$this->args = $args;
+		if(isset($query)) {
+            $this->query = $query;
+		}
+		if(isset($app)) {
+            $this->app = $app;
+		}
+		if(isset($routers)) {
+            $this->routers = $routers;
+		}
+		if(isset($resource)) {
+            $this->resource = $resource;
+		}
         $this->set();
     }
  
@@ -75,11 +98,25 @@ class Hook
             {
                 if(isset($value['vendor'])) {
                     $vendor = $value['vendor'];
-                    $hook = new $vendor();
-                    $hook->http($this->request, $this->response, $this->args, $this->query);
-                    $this->request = $hook->request();
-                    $this->response = $hook->response();
-                    $this->args = $hook->args();
+                    if (class_exists($vendor)) {
+                        $hook = new $vendor();
+                    } else {
+					    $this->logger = "Vendor {$vendor} недоступен";
+						//print_r($this->logger);
+					    return false;
+					}
+					if(method_exists($vendor,'http')) {
+                        $hook->http($this->request, $this->response, $this->args, $this->query);
+					}
+					if(method_exists($vendor,'request')) {
+                        $this->request = $hook->request();
+					}
+					if(method_exists($vendor,'response')) {
+                        $this->response = $hook->response();
+					}
+					if(method_exists($vendor,'args')) {
+                        $this->args = $hook->args();
+					}
                 }
             }
             return true;
@@ -88,7 +125,7 @@ class Hook
         }
     }
  
-    public function get($view = null, $render = null)
+    public function get($view = [], $render = null)
     {
         $this->view = $view;
         $this->render = $render;
@@ -98,30 +135,64 @@ class Hook
  
     public function post($resource = null, $name_db = null, $postQuery = null, array $postArr = [], $id = null)
     {
-        $this->resource = $resource;
-        $this->name_db = $name_db;
-        $this->postQuery = $postQuery;
-        $this->postArr = $postArr;
-        $this->id = $id;
+		if(isset($resource)) {
+            $this->id = $resource;
+		}
+		if(isset($name_db)) {
+            $this->name_db = $name_db;
+		}
+		if(isset($postQuery)) {
+            $this->postQuery = $postQuery;
+		}
+		if(isset($postArr)) {
+            $this->postArr = $postArr;
+		}
+		if(isset($id)) {
+            $this->id = $id;
+		}
         $this->run();
     }
  
     public function run()
     {
         $hooks = $this->hooks($this->query);
+		if((int)$this->print == 1) {
+		    print_r($hooks);
+		}
         if(isset($hooks[0])) {
             foreach($hooks as $value)
             {
                 if(isset($value['vendor'])) {
-                    $vendor = $value['vendor'];
-                    $hook = new $vendor();
+				    $vendor = $value['vendor'];
+                    if (class_exists($vendor)) {
+                        $hook = new $vendor();
+						if((int)$this->print == 1) {
+							print("vendor = {$vendor}<br>");
+						}
+                    } else {
+					    $this->logger = "Vendor {$vendor} недоступен";
+						if((int)$this->print == 1) {
+							print("logger - Vendor {$vendor} недоступен<br>");
+						}
+					    return false;
+					}
                     if ($this->query == 'GET') {
-                        $hook->get($this->view, $this->render);
-                        $this->view = $hook->view();
-                        $this->render = $hook->render();
+						if(method_exists($vendor,'get')) {
+                            $hook->get($this->view, $this->render);
+						}
+						if(method_exists($vendor,'view')) {
+                            $this->view = $hook->view();
+						}
+						if(method_exists($vendor,'render')) {
+                            $this->render = $hook->render();
+						}
                     } elseif ($this->query == 'POST') {
-                        $hook->post($this->resource, $this->name_db, $this->postQuery, $this->postArr, $this->id);
-                        $this->callback = $hook->callback($this->callback);
+						if(method_exists($vendor,'post')) {
+                            $hook->post($this->resource, $this->name_db, $this->postQuery, $this->postArr, $this->id);
+						}
+						if(method_exists($vendor,'callback')) {
+							$this->callback = $hook->callback($this->callback);
+						} 
                     }
                 }
             }
@@ -136,32 +207,58 @@ class Hook
     {
         $hooks = [];
         $hook = null;
-        foreach($this->config['hooks'] as $key => $value)
+        foreach($this->config['hooks']['vendor'] as $key => $value)
         {
-            if (isset($value['state']) && $value['state'] == '1') {
+		    $run = false;
+            if (isset($value['state']) && (int)$value['state'] == 1) {
                 if ($value['app'] == $this->app || $value['app'] == 'all') {
-                    if (isset($value['render']) && $value['render'] != '' && $value['render'] != ' ') {
+                    if (isset($value['render']) && $value['render'] != '' && $value['render'] != ' ' && (int)$value['render'] != 0) {
                         if($value['query'] == $query && $value['render'] == $this->render) {
-                            $hook['vendor'] = $value['vendor'];
-                            $hooks[] = $hook;
+                            $run = true;
+							if((int)$this->print == 1) {
+							    print("run = {$run} - true - 5<br>");
+							}
                         } elseif ($value['query'] == $query && $value['render'] == 'all') {
-                            $hook['vendor'] = $value['vendor'];
-                            $hooks[] = $hook;
+                            $run = true;
+							if((int)$this->print == 1) {
+							    print("run = {$run} - true - 4<br>");
+							}
                         } elseif ($value['query'] == 'all' && $value['render'] == 'all') {
-                            $hook['vendor'] = $value['vendor'];
-                            $hooks[] = $hook;
+                            $run = true;
+							if((int)$this->print == 1) {
+							    print("run = {$run} - true - 3<br>");
+							}
                         }
                     } else {
                         if($value['query'] == $query) {
-                            $hook['vendor'] = $value['vendor'];
-                            $hooks[] = $hook;
+                            $run = true;
+							if((int)$this->print == 1) {
+								print("run = {$run} - true - 2<br>");
+							}
                         } elseif ($value['query'] == 'all') {
-                            $hook['vendor'] = $value['vendor'];
-                            $hooks[] = $hook;
+                            $run = true;
+							if((int)$this->print == 1) {
+							    print("run = {$run} - true - 1<br>");
+							}
                         }
                     }
                 }
             }
+ 
+            if($run === true) {
+                $hook['vendor'] = $value['vendor'];
+                $hooks[] = $hook;
+            }
+        }
+		
+        if($run === true) {
+			if((int)$this->print == 1) {
+                print("run = {$run} - true - 7<br>");
+			}
+        } else {
+		    if((int)$this->print == 1) {
+                print("run = {$run} - false - 8<br>");
+			}
         }
  
         return $hooks;
